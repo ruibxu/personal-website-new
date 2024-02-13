2d73039dc473646097b013c8aeea1199bd21a6d2
Ruibo Xu <ruibo.xu@stonybrook.edu>
2024-02-13 11:47:16 -0500

    bug fix

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -21,8 +21,8 @@ const Intro = () => {
         </div>
       <div className="flex-1 flex flex-col items-start justify-center space-y-4">
         <div>
-          <h1 className="md:text-5xl font-bold text-3xl">Hi, I'm Ruibo Xu</h1>
-          <p className="md:text-2xl py-5 text-lg">I'm a software engineer.</p>
+          <h1 className="md:text-5xl font-bold text-3xl">Hi, I&#39;m Ruibo Xu</h1>
+          <p className="md:text-2xl py-5 text-lg">I&#39;m a software engineer.</p>
         </div>
         <Link href='./Resume.pdf' target="_blank">
         <button className="px-8 py-4 bg-gray-800 text-white rounded hover:bg-slate-600 text-2xl">
